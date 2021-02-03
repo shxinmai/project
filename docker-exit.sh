c45b9edd92a90201f686532f6c1912a60ff71bb1
周龙 <1033085514@qq.com>
2021-02-03 14:32:28 +0800

    ASFDASFD

--- docker-exit.sh
+++ docker-exit.sh
@@ -1,10 +1,8 @@
- 
-docker ps |£¨grep netcore_testweb | grep netcore_testadmin£© &> /dev/null 
-if [ $? -ne 0 ]
-then
+if [[ $(docker-compose -f /data/jenkins_home/workspace/Test/docker-compose.yml ps | grep "netcore" | wc -l) -ne 0 ]]
+then 
     echo "core is not up,we will start up it!!!"
     docker-compose -f /data/jenkins_home/workspace/Test/docker-compose.yml up -d
 else
     echo "core is up!!! we will stop it !!!!!"
-   docker-compose -f /data/jenkins_home/workspace/Test/docker-compose.yml down
+    docker-compose -f /data/jenkins_home/workspace/Test/docker-compose.yml down
 fi
