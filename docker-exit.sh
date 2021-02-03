7ac45dacb70d5dacd6609abbe6f8613be3e9c320
周龙 <1033085514@qq.com>
2021-02-03 14:24:27 +0800

    啊打发十分的

--- docker-exit.sh
+++ docker-exit.sh
@@ -1,10 +1,10 @@
  
-docker ps | grep netcore_testweb | grep netcore_testadmin &> /dev/null 
+docker ps | grep netcore_testweb &> /dev/null 
 if [ $? -ne 0 ]
 then
-   echo "core is up!!! we will stop it !!!!!"
-   docker-compose -f /data/jenkins_home/workspace/Test/docker-compose.yml down
-else
     echo "core is not up,we will start up it!!!"
     docker-compose -f /data/jenkins_home/workspace/Test/docker-compose.yml up -d
+else
+    echo "core is up!!! we will stop it !!!!!"
+   docker-compose -f /data/jenkins_home/workspace/Test/docker-compose.yml down
 fi
