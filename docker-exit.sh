e32d8463eceaca811e52cbd54290e138892e95a6
周龙 <1033085514@qq.com>
2021-02-03 14:43:49 +0800

    花木城

--- docker-exit.sh
+++ docker-exit.sh
@@ -1,11 +1,15 @@
  
-docker-compose -f /data/jenkins_home/workspace/Test/docker-compose.yml ps | grep "netcore" | wc -l
-if [ $? -ne 0 ]
+a= $(docker-compose -f /data/jenkins_home/workspace/Test/docker-compose.yml ps | grep "netcore" | wc -l)
+if [ $a <=0 ]
 then
     echo "core is not up,we will start up it!!!"
     docker-compose -f /data/jenkins_home/workspace/Test/docker-compose.yml up -d
 else
     echo "core is up!!! we will stop it !!!!!"
-   docker-compose -f /data/jenkins_home/workspace/Test/docker-compose.yml down
+    docker-compose -f /data/jenkins_home/workspace/Test/docker-compose.yml down
+    echo "next we will remove docker images !!!!!!"
+    docker rmi --force `docker images | grep "test" | awk '{print $3}'`
+    echo "the end we will start up "
+    docker-compose -f /data/jenkins_home/workspace/Test/docker-compose.yml up -d
 fi
 
